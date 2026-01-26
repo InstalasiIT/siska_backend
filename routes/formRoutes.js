3d406323f5e0cb6be0d35ad7556e0b4e5673b3b5
fadetul_f <fadetulfitriyeh.id@gmail.com>
2026-01-26 09:48:32 +0700

    auth

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -9,27 +9,22 @@ export default function userRoute() {
 
   formRoutes.get(
     "/naik-kelas/get-datapasien/:no_bpjs",
-    authMiddleware,
     controller.getPasien()
   );
   formRoutes.get(
     "/naik-kelas/get-pasien/:no_bpjs",
-    authMiddleware,
     controller.getDataPasien(),
   );
   formRoutes.get(
     "/naik-kelas/get-kelas",
-    authMiddleware,
     controller.getKelas()
   );
   formRoutes.post(
     "/naik-kelas/save",
-    authMiddleware,
     controller.save()
   );
   formRoutes.post(
     "/rawat-inap-penuh/save",
-    authMiddleware,
     controller.saveKelasPenuh()
   );
 
